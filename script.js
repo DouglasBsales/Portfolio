a8c671cfdb2de587e980ad1adf0591f6174c15b6
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-01-30 23:30:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let iconeMenuNormal = document.getElementById('meuMobile')
+iconeMenuNormal.addEventlistener('click', function(){
+console.log('click no ícone do menu')
+    iconeMenuNormal.src = 'src/public/x.svg'})
