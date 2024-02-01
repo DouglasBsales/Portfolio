41909b0b09be6a53f62292bc3197e040fb847d82
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-02-01 16:09:04 -0300

    added menuMobile and its logic and some changes in css and html

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-function menuShow(){
-    let iconeMenu = document.getElementById('menuMobile');
-    if(iconeMenu.src.includes('menu.svg')){
-        iconeMenu.src = "public/x.svg"
-    }
-    else {
-        iconeMenu.src = 'public/menu.svg'
-    }
+function menuShow() {
+  let exibirMenu = document.getElementById("pageMenuMobile");
+  exibirMenu.style.visibility = "visible";
+  exibirMenu.style.width = "330px"
+  exibirMenu.style.transition = "width 0.4s ease, visibility 0.2s"
+
+  let pageOpenBody = document.getElementById('pageSobreBody')
+  pageOpenBody.style.visibility = 'visible'
+  pageOpenBody.style.opacity = "1"
+  pageOpenBody.style.transition = "opacity 0.3s ease"
 }
+
+function fecharPage(){
+    let exibirMenu = document.getElementById("pageMenuMobile");
+    exibirMenu.style.visibility = "hidden";
+    exibirMenu.style.width= "88px"
+
+    let pageOpenBody = document.getElementById('pageSobreBody')
+    pageOpenBody.style.visibility = "hidden"
+    pageOpenBody.style.opacity = "0"
+}
+
+let bgLink = document.getElementById('link1');
+    bgLink.addEventListener('click', ()=>{
+    bgLink.style.backgroundColor = "#445964"
+    bgLink.style.color = "#fff";
+})
+
+
