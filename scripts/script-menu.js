ad1abe85d4a87e0d6541bfa5b9e3c0f56516ec06
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-02-28 21:45:53 -0300

    att

--- scripts/script-menu.js
+++ scripts/script-menu.js
@@ -1,29 +1,25 @@
 function menuShow() {
   let exibirMenu = document.getElementById("pageMenuMobile");
   exibirMenu.style.visibility = "visible";
-  exibirMenu.style.width = "330px"
-  exibirMenu.style.transition = "width 0.4s ease, visibility 0.2s"
+  exibirMenu.style.width = "330px";
+  exibirMenu.style.transition = "width 0.5s ease, visibility 0.3s";
 
-  let pageOpenBody = document.getElementById('pageSobreBody')
-  pageOpenBody.style.visibility = 'visible'
-  pageOpenBody.style.opacity = "1"
-  pageOpenBody.style.transition = "opacity 0.3s ease"
+  let pageOpenBody = document.getElementById("pageSobreBody");
+  pageOpenBody.style.visibility = "visible";
+  pageOpenBody.style.opacity = "1";
+  pageOpenBody.style.transition = "opacity 0.5s ease";
 }
 
-function fecharPage(){
-    let exibirMenu = document.getElementById("pageMenuMobile");
-    exibirMenu.style.visibility = "hidden";
-    exibirMenu.style.width= "88px"
+function fecharPage() {
+  let exibirMenu = document.getElementById("pageMenuMobile");
+  exibirMenu.style.visibility = "hidden";
+  exibirMenu.style.width = "88px";
 
-    let pageOpenBody = document.getElementById('pageSobreBody')
-    pageOpenBody.style.visibility = "hidden"
-    pageOpenBody.style.opacity = "0"
+  let pageOpenBody = document.getElementById("pageSobreBody");
+  pageOpenBody.style.visibility = "hidden";
+  pageOpenBody.style.opacity = "0";
 }
 
-let bgLink = document.getElementById('link1');
-    bgLink.addEventListener('click', ()=>{
-    bgLink.style.backgroundColor = "#445964"
-    bgLink.style.color = "#fff";
-})
-
-
+let bgLink = document.getElementById("link1");
+bgLink.style.backgroundColor = "#445964";
+bgLink.style.color = "#fff";
